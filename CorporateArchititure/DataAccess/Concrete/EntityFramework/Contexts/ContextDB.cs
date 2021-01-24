dcc2769ea85d43d9ebc7bfc217791d0efee824c2
alican <64075530+ekincialican@users.noreply.github.com>
2021-01-24 14:11:18 +0300

    the project upload to github

---
+++ CorporateArchititure/DataAccess/Concrete/EntityFramework/Contexts/ContextDB.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public  class ContextDB:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString:   @"Server=(localdb)\MSSQLLocalDB;
+DataBase=BackEndDb;Trusted_Connection=true");
+
+        }
+        public DbSet<Product> products { get; set; }
+    }
+}
---
+++ CorporateArchititure/Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product : IEntity
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int CategoryId { get; set; }
+        public string QuantityPerUnity { get; set; }
+        public decimal UnityPrice { get; set; }
+        public short UnityStock { get; set; }
+    }
+}
